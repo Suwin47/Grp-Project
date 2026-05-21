2e474cf2e2ae52b2bc0338e385c222034ba5e0cd
Abuthakeer-abu <abuthakeer5939@gmail.com>
2026-05-21 20:03:56 +0530

    updated restaurant page

---
+++ restaurant_page.js
@@ -0,0 +1,27 @@
+//All Card Selected
+
+const cards = document.querySelectorAll(".card");
+
+//First card
+
+cards[0].addEventListener("click",function(){
+    window.location.href = "adayar.html";
+})
+
+//Second card
+
+cards[1].addEventListener("click",function(){
+    window.location.href = "anjappar.html";
+})
+
+//Third card
+
+cards[2].addEventListener("click",function(){
+    window.location.href = "burgerking.html";
+})
+
+//Fourth card
+
+cards[3].addEventListener("click",function(){
+    window.location.href = "dominos.html";
+})
